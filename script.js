1f2b145b8466dbcdb54082541971e6be825e2238
nicolorenzo <nicolorenzo@live.com>
2022-06-13 18:44:19 -0300

    Adicionando o icone ao card segundo a escolha do usuário

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
-// Pegando os elemntos do form
+// Selecionando os elemntos do form
 let elementos = {
   titulo: document.querySelector('#titulo'),
   imagem: document.querySelector('#imagem'),
   descricao: document.querySelector('#descricao'),
-  // Criando um array com as opções do form
-  escolha: document.getElementsByName('escolha'),
 };
 
 // Selecionando o form
 let form = document.querySelector('form');
 
 // Selecionando a div que contem os cards
-let cards = document.querySelector('.card-wraper');
+let cards = document.querySelector('.card-wrapper');
 
-// Prevenindo o padrão do botao submit e adicionando o card ao html
 form.addEventListener('submit', (evento) => {
   // Previne o comportamento padrão do botão submit
   evento.preventDefault();
 
-  // Percorre o array contendo as opções do form e armazena a escolha em uma variavel
-  for (i = 0; i < elementos.escolha.length; i++) {
-    if (elementos.escolha[i].checked) escolha2 = elementos.escolha[i].value;
+  // Selecionando a escolha do usuário
+  let escolha = document.querySelector('input[name="escolha"]:checked');
+
+  // Verificando e atribuindo o logotipo referente a escolha
+  if (escolha.value == 'DC') {
+    logo =
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/DC_Comics_logo.svg/1024px-DC_Comics_logo.svg.png';
+  } else {
+    logo =
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/2560px-Marvel_Logo.svg.png';
   }
 
-  // Adicionando o que foi digitado no form ao html
+  // Adicionando o que foi digitado no form ao card
   cards.innerHTML += `
-    <div class="card">
+    <div class="card aliceblue">
       <h2>
         ${elementos.titulo.value}
       </h2>
 
       <img src="${elementos.imagem.value}" alt="">
       <p>
-        ${escolha2}
+      <img class="logo" src="${logo}" alt="">
       </p>
       <p>
         ${elementos.descricao.value}
